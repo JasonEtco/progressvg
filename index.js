379b1c452ca864b34131deb52e7f14941c0ab700
Jason Etcovitch <jasonetco@github.com>
2018-04-21 15:36:25 -0400

    Set content-type

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const template = require('./template')
 module.exports = app => {
   app.get('/progress', (req, res) => {
     const { p, m } = req.query
+    res.setHeader('Content-Type', 'image/svg+xml')
     res.send(template(p, m))
   })
 }
