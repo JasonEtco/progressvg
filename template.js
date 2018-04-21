73b1963f416d5e794d8d8676d22427536a4cfba1
Jason Etcovitch <jasonetco@github.com>
2018-04-21 15:42:03 -0400

    Add fontFamily option

--- template.js
+++ template.js
@@ -2,6 +2,7 @@ module.exports = (progress, max, options) => {
   const opts = {
     background: '#EEEEEE',
     foreground: '#34d058',
+    fontFaimly: '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif',
     width: 668,
     height: 6,
     paddingX: 17,
@@ -19,10 +20,10 @@ module.exports = (progress, max, options) => {
               <rect id="Rectangle" fill="${opts.foreground}" x="0" y="19" width="0" height="${opts.height}" rx="2">
                 <animate attributeName="width" begin="0.5s" dur="600ms" from="0" to="${percentage}%" repeatCount="1" fill="freeze" calcMode="spline" keyTimes="0; 1" keySplines="0.3, 0.61, 0.355, 1"/>
               </rect>
-              <text id="Option-A" font-family="-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif" font-size="12" font-weight="bold" letter-spacing="1" fill="#212529">
+              <text id="Option-A" font-family="${opts.fontFamily}" font-size="12" font-weight="bold" letter-spacing="1" fill="#212529">
                 <tspan x="0" y="12">Progress</tspan>
               </text>
-              <text x="${opts.width - (opts.paddingX * 2)}" y="12" id="150" text-anchor="end" font-family="-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif" font-size="12" font-weight="normal" letter-spacing="1" fill="#868E96">
+              <text x="${opts.width - (opts.paddingX * 2)}" y="12" id="150" text-anchor="end" font-family="${opts.fontFamily}" font-size="12" font-weight="normal" letter-spacing="1" fill="#868E96">
                 <tspan>${progress} of ${max}</tspan>
               </text>
           </g>
