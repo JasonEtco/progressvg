866a107d5c683148b0dd78af3c18fc8a8975b6ea
Jason Etcovitch <jasonetco@github.com>
2018-04-21 15:48:35 -0400

    Set cache-control header

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = app => {
   app.get('/progress', (req, res) => {
     const { p, m } = req.query
     res.setHeader('Content-Type', 'image/svg+xml')
+    res.setHeader('Cache-Control', 'no-cache')
     res.send(template(p, m))
   })
 }
